36318afa0ee2ccdf62200bbd61896189688a76c7
Raul La Rosa <rlarosa@scorbot.com>
2021-11-09 22:29:03 -0800

    messing around with scala

--- src/script/scala/progscala3/introscala/Upper1.scala
+++ src/script/scala/progscala3/introscala/Upper1.scala
@@ -2,8 +2,12 @@
 
 class Upper1:
   def convert(strings: Seq[String]): Seq[String] =
-    strings.map((s: String) => s.toUpperCase)
+    strings.map((s: String) => s.toLowerCase)
 
 val up = new Upper1()
-val uppers = up.convert(List("Hello", "World!"))
+val uppers = up.convert(List("Hello", "World", "!"))
 println(uppers)
+
+uppers.foreach((upper: String) => println(upper.contains("el")))
+1 + 2
+val bool1: Boolean = true
